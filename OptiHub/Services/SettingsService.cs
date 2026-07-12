eb9ad9901953b52495532b25a910ccb227c660f3
Erix <147302990+BarcusEric@users.noreply.github.com>
2026-07-11 22:09:23 -0500

    v1.9.10: About app version + liability disclaimer

--- OptiHub/Services/SettingsService.cs
+++ OptiHub/Services/SettingsService.cs
@@ -93,7 +93,7 @@ public sealed class SettingsService
 
         if (string.IsNullOrWhiteSpace(settings.DiscordKitVersion))
         {
-            settings.DiscordKitVersion = "1.3.12";
+            settings.DiscordKitVersion = "1.3.13";
             changed = true;
         }
 
